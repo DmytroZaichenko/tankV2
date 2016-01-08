d8b3f64786aa2fd9cfaa1a4a5c39651157e0a308
Dmytro Zaichenko <dim2002@ukr.net>
2016-01-08 15:16:53 +0200

    with water images

--- src/ua/tankv2/field/Water.java
+++ src/ua/tankv2/field/Water.java
@@ -1,6 +1,7 @@
 package ua.tankv2.field;
 
-import java.awt.Color;
+import java.awt.*;
+import java.awt.image.ImageObserver;
 
 public class Water extends SimpleBFObject {
 
@@ -9,4 +10,18 @@ public class Water extends SimpleBFObject {
         color = new Color(0, 185, 255);
     }
 
+    @Override
+    public void draw(Graphics g) {
+        Graphics2D g2D = (Graphics2D)g;
+        Composite org = g2D.getComposite();
+        Composite translucent = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
+        g2D.setComposite(translucent);
+        g.drawImage(image, getX(), getY(), getX() + SIZE_QUADRANT, getY() + SIZE_QUADRANT, 96, 32, 128, 64, new ImageObserver() {
+            @Override
+            public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+                return false;
+            }
+        });
+        g2D.setComposite(org);
+    }
 }
